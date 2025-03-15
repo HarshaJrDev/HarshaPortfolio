2b9e2aefb718ce0a7bd2031a97899720d8f523f5
Harsha Ravuri <development1043@gmail.com>
2025-03-15 18:35:29 +0530

    Updated project details

--- src/app/Layout/About.js
+++ src/app/Layout/About.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { FaLinkedin, FaGithub, FaDownload, FaEye } from "react-icons/fa";
-import Image from "next/image";
+
 
 const About = () => {
   return (

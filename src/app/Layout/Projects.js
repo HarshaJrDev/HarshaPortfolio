f94aa84aae6deaa7f291ffa5637a502010e647be
Harsha Ravuri <development1043@gmail.com>
2025-03-15 17:42:34 +0530

    FIx Header

--- src/app/Layout/Projects.js
+++ src/app/Layout/Projects.js
@@ -26,9 +26,7 @@ const projects = [
 
 const Projects = () => {
   return (
-    <div className="p-10 bg-gray-900 text-white min-h-screen">
-      <h2 className="text-4xl font-bold mb-10 text-center">Portfolio</h2>
-      
+    <div className="p-10 bg-gray-900 text-white min-h-screen">      
       <h3 className="text-3xl font-semibold mb-6 text-center">Projects</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project, index) => (
